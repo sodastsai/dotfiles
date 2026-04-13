977d001c90b43c1c1bfa352a48fd0482d8e5a1e4
Tien-Che Tsai <sodas@icloud.com>
2026-04-13 20:36:26 +0000

    Update zsh setup

--- devenv/.devcontainer/setup.sh
+++ devenv/.devcontainer/setup.sh
@@ -15,6 +15,17 @@ fi
 
 # oh-my-zsh update
 log "--- oh-my-zsh ---"
+
+log "installing fzf"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+log "installing zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+log "installing zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   log "installing oh-my-zsh"
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,17 +1,25 @@
-# Zsh setup
+# fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# zsh setup
 export ZSH="$HOME/.oh-my-zsh"
 ZSH_THEME="af-magic"
-
-plugins=(git virtualenv)
+plugins=(
+    branch
+    dotenv
+    fzf
+    git
+    uv
+    virtualenv
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+)
 VIRTUAL_ENV_DISABLE_PROMPT=0
-
 source $ZSH/oh-my-zsh.sh
 
-
-# Tools env
+# env
 export EDITOR="code --wait"
 
-
-# Aliases
+# aliases
 alias ls="ls -h --color"
 alias lll="ls -al"
